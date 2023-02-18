ab9efeba738980cbfb3ed2077171193417541094
John Martin <john.itvn@gmail.com>
2023-02-18 13:17:37 +0700

    chore: initialize repository

--- packages/qc/index.js
+++ packages/qc/index.js
@@ -8,6 +8,7 @@ const eslint = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
   ],
 };
 module.exports = { eslint };
