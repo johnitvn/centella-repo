de773c2138c98addb20cf081a4b6547c366f4269
John Martin <john.itvn@gmail.com>
2023-02-18 14:00:23 +0700

    chore: initialize repository

--- packages/svelte-qc/index.js
+++ packages/svelte-qc/index.js
@@ -2,14 +2,12 @@
  * @type {import("eslint").Linter.Config}
  */
 const eslint = {
-    "plugins": [
-        "svelte3"
-    ],
-    "overrides": [
-        {
-            "files": ["*.svelte"],
-            "processor": "svelte3/svelte3"
-        }
-    ]
-}
-module.exports = eslint
+  plugins: ["svelte3"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+};
+module.exports = eslint;
